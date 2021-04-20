29970833119b1cf5df639109dbcf231ab949cb56
Jonathan Nilsson <jonathan@voysys.se>
2021-04-20 18:00:43 +0200

    Partial read

--- examples/test.rs
+++ examples/test.rs
@@ -1,14 +1,14 @@
-use std::{env, error::Error};
-
 use mdns_client::MdnsClient;
+use std::{env, error::Error, thread, time::Duration};
 
 fn main() -> Result<(), Box<dyn Error>> {
     env::set_var("RUST_LOG", "info");
     env_logger::init();
 
-    let mdns = MdnsClient::new()?;
+    let mdns = MdnsClient::new("OdenConfigurator._oden_configurator._tcp.local")?;
 
-    loop {}
-
-    Ok(())
+    loop {
+        println!("{:#?}", mdns.get_services());
+        thread::sleep(Duration::from_millis(2000));
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,17 @@
+use dns_parser::{rdata::Srv, Packet, RData, ResourceRecord};
 use std::{
+    collections::HashMap,
     error::Error,
     io::{self},
     net::{Ipv4Addr, SocketAddr, UdpSocket},
-    sync::mpsc::{sync_channel, RecvTimeoutError, SyncSender},
+    sync::{
+        mpsc::{sync_channel, RecvTimeoutError, SyncSender},
+        Arc, Mutex,
+    },
     thread::{self, JoinHandle},
-    time::Duration,
+    time::{Duration, Instant},
 };
 
-use dns_parser::Packet;
 #[cfg(not(target_os = "windows"))]
 use net2::unix::UnixUdpBuilderExt;
 
@@ -30,11 +34,11 @@ fn create_socket() -> io::Result<std::net::UdpSocket> {
         .bind((ADDR_ANY, MULTICAST_PORT))
 }
 
-pub fn send_request(socket: &UdpSocket) -> Result<(), Box<dyn Error>> {
+pub fn send_request(socket: &UdpSocket, service: &str) -> Result<(), Box<dyn Error>> {
     let mut builder = dns_parser::Builder::new_query(0, false);
     let prefer_unicast = false;
     builder.add_question(
-        "OdenConfigurator._oden_configurator._tcp.local",
+        service,
         prefer_unicast,
         dns_parser::QueryType::SRV,
         dns_parser::QueryClass::IN,
@@ -48,30 +52,84 @@ pub fn send_request(socket: &UdpSocket) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-pub fn handle_response(packet: &Packet, from: SocketAddr) {
-    log::info!("{:?} => {:?}", from, packet);
+pub fn handle_response(
+    packet: &Packet,
+    from: SocketAddr,
+    service: &str,
+    database: &Mutex<HashMap<Service, ServiceRecord>>,
+) {
+    log::info!("{:?} => {:#?}", from, packet);
+
+    if packet.header.query {
+        return;
+    }
+
+    let mut database = database.lock().unwrap();
+
+    for answer in &packet.answers {
+        if let ResourceRecord {
+            name,
+            data: RData::SRV(Srv { target, port, .. }),
+            ..
+        } = answer
+        {
+            if name.to_string() == service {
+                let service = Service {
+                    host: target.to_string(),
+                    port: *port,
+                };
+
+                database
+                    .entry(service)
+                    .and_modify(|e| e.last_seen_time = Instant::now())
+                    .or_insert_with(|| ServiceRecord {
+                        last_seen_time: Instant::now(),
+                        addresses: Vec::new(),
+                    });
+            }
+        }
+    }
 }
 
-pub fn recive_response(socket: &UdpSocket) -> Result<(), Box<dyn Error>> {
+pub fn recive_response(
+    socket: &UdpSocket,
+    service: &str,
+    database: &Mutex<HashMap<Service, ServiceRecord>>,
+) -> Result<(), Box<dyn Error>> {
     let mut buffer: [u8; 2048] = [0; 2048];
 
     loop {
         let (count, from) = socket.recv_from(&mut buffer)?;
 
         match dns_parser::Packet::parse(&buffer[..count]) {
-            Ok(packet) => handle_response(&packet, from),
+            Ok(packet) => handle_response(&packet, from, service, database),
             Err(e) => log::warn!("{}", e),
         }
     }
 }
 
+#[derive(Clone, Debug, Hash, PartialEq, Eq)]
+pub struct Service {
+    host: String,
+    port: u16,
+}
+
+#[derive(Clone, Debug)]
+pub struct ServiceRecord {
+    last_seen_time: Instant,
+    addresses: Vec<SocketAddr>,
+}
+
 pub struct MdnsClient {
+    database: Arc<Mutex<HashMap<Service, ServiceRecord>>>,
     exit_tx: SyncSender<()>,
     thread: Option<JoinHandle<()>>,
 }
 
 impl MdnsClient {
-    pub fn new() -> Result<MdnsClient, Box<dyn Error>> {
+    pub fn new(service: &str) -> Result<MdnsClient, Box<dyn Error>> {
+        let database = Arc::new(Mutex::new(HashMap::new()));
+
         let socket = create_socket()?;
 
         socket.set_multicast_loop_v4(false)?;
@@ -80,20 +138,35 @@ impl MdnsClient {
 
         let (exit_tx, exit_rx) = sync_channel(0);
 
-        let thread = thread::spawn(move || loop {
-            match exit_rx.recv_timeout(Duration::from_secs(1)) {
-                Ok(()) | Err(RecvTimeoutError::Disconnected) => break,
-                Err(RecvTimeoutError::Timeout) => {
-                    send_request(&socket).ok();
-                    recive_response(&socket).ok();
+        let thread = thread::spawn({
+            let service = service.to_string();
+            let database = database.clone();
+
+            move || loop {
+                match exit_rx.recv_timeout(Duration::from_secs(1)) {
+                    Ok(()) | Err(RecvTimeoutError::Disconnected) => break,
+                    Err(RecvTimeoutError::Timeout) => {
+                        send_request(&socket, &service).ok();
+                        recive_response(&socket, &service, &database).ok();
+                    }
                 }
             }
         });
 
         Ok(MdnsClient {
+            database,
             exit_tx,
             thread: Some(thread),
         })
+    }
+
+    pub fn get_services(&self) -> Vec<(Service, ServiceRecord)> {
+        self.database
+            .lock()
+            .unwrap()
+            .iter()
+            .map(|(service, record)| (service.clone(), record.clone()))
+            .collect()
     }
 }
 
